f78d529e0b9666ebc6c04fe8c518789eecbe1e2f
Calvin Chen <chencalvin99@berkeley.edu>
2019-03-10 14:38:13 -0700

    wefew

--- src/js/index.js
+++ src/js/index.js
@@ -371,6 +371,7 @@ class App extends React.Component {
             <h2>Vote for the next ass</h2>
 
             <h2> I EAT ASSSS!!!!!!! YAYYYY</h2>
+            <h2> I EAT potato!!!!!!! YAYYYY</h2>
 
             <label>
                <b>How much Ether do you want to bet? <input className="bet-input" ref="ether-bet" type="number" placeholder={this.state.minimumBet}/></b> ether
