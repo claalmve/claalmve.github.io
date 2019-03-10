7881d99daae444d7e586452019f3088f62f385da
Calvin Chen <chencalvin99@berkeley.edu>
2019-03-10 15:16:58 -0700

    :wefwef

--- src/js/index.js
+++ src/js/index.js
@@ -368,7 +368,7 @@ class App extends React.Component {
 
             <hr></hr>
 
-            <h2>Vote for the next number in this sequence of numbers</h2>
+            <h2>Vote for the next number</h2>
 
             <label>
                <b>How much Ether do you want to bet? <input className="bet-input" ref="ether-bet" type="number" placeholder={this.state.minimumBet}/></b> ether
@@ -384,8 +384,8 @@ class App extends React.Component {
                <li>6</li>
                <li>7</li>
                <li>8</li>
-               <li>9</li>
-               <li>10</li>
+               // <li>9</li>
+               // <li>10</li>
             </ul>
 
             <hr></hr>
